2763ca3539e7d604c428f75aba88a48a1afab908
Reinaltt <reinalttinho@outlook.com>
2025-09-27 01:25:54 +0000

    Criei minha V1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,46 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado;
+    char codigoCarta[10];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTur;
 
   // Área para entrada de dados
+    printf("Seja bem-vindo(a) ao SuperTrunfo Países!\n Vamos cadastrar a primeira carta do jogo!\n");
+    printf("Digite uma letra entre A e H para representar o Estado:\n");
+    scanf("%c", &estado);
 
+    printf("Digite um código (máximo de 3 caracteres) para sua carta (letras e números):\n");
+    scanf(" %c", &codigoCarta);
+
+    printf("Digite o nome da Cidade:\n");
+    scanf(" %s", cidade);
+
+    printf("Informe a População da cidade:\n");
+    scanf("%d", &populacao);
+
+    printf("Informe a Area da cidade (em km²):\n");
+    scanf("%f", &area);
+
+    printf("Informe o PIB da cidade (em R$):\n");
+    scanf("%f", &pib);
+
+    printf("Informe o total de Pontos Turísticos da cidade:\n");
+    scanf("%d", &pontosTur);
   // Área para exibição dos dados da cidade
+    printf("Primeira Carta cadastrada com sucesso! Confira os dados:\n");
+    printf("ESTADO: %c\n", estado);
+    printf("CÓDIGO DA CARTA: %c\n", codigoCarta);
+    printf("CIDADE: %s\n", cidade);
+    printf("ESTADO: %d\n", populacao);
+    printf("ESTADO: %f\n", area);
+    printf("ESTADO: %f\n", pib);
+    printf("ESTADO: %d\n", pontosTur);
 
 return 0;
-} 
+
+}
